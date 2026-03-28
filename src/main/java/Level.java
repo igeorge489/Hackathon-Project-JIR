55886fceb4983b765e86c4bc83951d3f8a980ef3
Irene George <igeorge489@student.fuhsd.org>
2026-03-28 14:17:41 -0700

    added hitbox (#7)

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -2,38 +2,42 @@
  * @author rayna_li
  * This class represents the levels that the game has
  */
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
 public class Level {
+    private int levelNumber;
+    private String imageFileName;
+    private List<Rectangle> trashHitboxes; // Stores where the trash is
+    private long startTime;
+    private boolean isCleared;
 
-	private int currentLevel;
-	
-	private long startTime;  
-	
-	private boolean levelStatus;
+    public Level(int levelNumber, String imageFileName) {
+        this.levelNumber = levelNumber;
+        this.imageFileName = imageFileName;
+        this.trashHitboxes = new ArrayList<>();
+        this.startTime = System.currentTimeMillis();
+        this.isCleared = false;
+    }
 
-	public Level(int currentLevel) {
-		this.currentLevel = currentLevel;
-		startTime = System.currentTimeMillis();
-		levelStatus = false;
-	}
-	
-	public int getCurrentLevel() {
-		return currentLevel;
-	}
-	
-	public void setCurrentLevel(int lvl) {
-		currentLevel = lvl;
-	}
-	
-	public int getTime() {
-		long timeFinished = System.currentTimeMillis();
-		return (int)((timeFinished - startTime)/1000);
-	}
-	
-	public void setLevelStatus(boolean status) {
-		levelStatus = status;
-	}
-	
-	public boolean getLevelStatus() {
-		return levelStatus;
-	}
+    // Add a hitbox found in the JSON
+    public void addHitbox(int x, int y, int width, int height) {
+        trashHitboxes.add(new Rectangle(x, y, width, height));
+    }
+
+    // Check if the user clicked on trash
+    public boolean checkClick(int clickX, int clickY) {
+        for (Rectangle box : trashHitboxes) {
+            if (box.contains(clickX, clickY)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getImageFileName() { return imageFileName; }
+    
+    public int getTime() {
+        return (int)((System.currentTimeMillis() - startTime) / 1000);
+    }
 }
